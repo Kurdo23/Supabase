ae6c48390e124b3792b005a5f8cdd4dfa69dab21
E170732 <a.sokolik@student.helmo.be>
2025-11-28 15:28:20 +0100

    initial commit

---
+++ test.ts
@@ -0,0 +1,22 @@
+import { createClient } from '@supabase/supabase-js'
+import { env } from './utils/envConfig'
+
+
+const supabase = createClient(env.supabaseUrl, env.supabaseAnonKey)
+
+async function callFunction() {
+    const { data, error } = await supabase.functions.invoke('hello-world', {
+        body: { name: 'Functions' }
+    })
+
+    if (error) {
+        console.error('Error:', error)
+        return
+    }
+
+    console.log('Success:', data)
+    return data
+}
+
+// Appeler la fonction
+callFunction()
